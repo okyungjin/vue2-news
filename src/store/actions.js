41e69784af7a98786682dac0080a41c1863add29
okyungjin <okyungjin@gmail.com>
2022-04-05 12:01:17 +0900

    Tab 전환 시 Spinner가 보여지지 않는 이슈 수정

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,21 +4,21 @@ import {
 
 export default {
   FETCH_LIST({ commit }, routeName) {
-    fetchList(routeName)
+    return fetchList(routeName)
       .then(({ data }) => {
         commit('SET_LIST', data);
       })
       .catch((err) => throw new Error(err));
   },
   FETCH_USER({ commit }, userName) {
-    fetchUser(userName)
+    return fetchUser(userName)
       .then(({ data }) => {
         commit('SET_USER', data);
       })
       .catch((err) => throw new Error(err));
   },
   FETCH_ITEM({ commit }, itemId) {
-    fetchItem(itemId)
+    return fetchItem(itemId)
       .then(({ data }) => {
         commit('SET_ITEM', data);
       })
