65fa1aec2d6a6bdfb3a78bedf90d561b2a5382ca
okyungjin <okyungjin@gmail.com>
2022-04-06 09:22:29 +0900

    [Plugins] chart.js를 Plugin으로 등록하기

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './routes';
 import { store } from './store/index';
+import chartPlugin from './plugins/chartPlugin';
 
 Vue.config.productionTip = false;
+
+Vue.use(chartPlugin);
 
 new Vue({
   render: (h) => h(App),
